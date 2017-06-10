a9425c27b6d303c0ef1147d20f480fcbd5d70457
Nick Davis <hire@nicholasdavis.co>
2017-06-10 18:40:44 -0400

    Added search field added close class overriding ui-bootstrap btn

--- app/scripts/components/gallery/gallery-controller.js
+++ app/scripts/components/gallery/gallery-controller.js
@@ -1,15 +1,15 @@
 ﻿//GalleryController
 galleryModule.controller('GalleryController', ['$scope', '$state', '$stateParams', 'GalleryService', 'AlertService',
     function ($scope, $state, $stateParams, GalleryService, AlertService) {
-        AlertService.alerts.add('danger', 'Something went wrong', 20000)
-        //
+
+        //Loading gallery
         $scope.isLoading = true;
         GalleryService.getGalleryConfig().then(function (response) {
             console.log(response)
             $scope.gallery = response.photos;
         }).catch(function () {
             //Display alert for user 
-            AlertService.alerts.add('danger', 'Something went wrong', 20000)
+            AlertService.alerts.add('danger', 'Something went wrong', 20000);
         }).finally(function () {
             //Hide loader
             $scope.isLoading = false;
