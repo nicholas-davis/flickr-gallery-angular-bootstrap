1e0014f9776c5ce950b361c7c8adc8a0938d2b4c
Nick Davis <hire@nicholasdavis.co>
2017-06-10 20:58:19 -0400

    finailzing flickr photo search

--- app/scripts/components/gallery/gallery-controller.js
+++ app/scripts/components/gallery/gallery-controller.js
@@ -2,18 +2,36 @@
 galleryModule.controller('GalleryController', ['$scope', '$state', '$stateParams', 'GalleryService', 'AlertService',
     function ($scope, $state, $stateParams, GalleryService, AlertService) {
 
-        //Loading gallery
-        $scope.isLoading = true;
-        GalleryService.getGalleryConfig().then(function (response) {
-            console.log(response)
-            $scope.gallery = response.photos;
-        }).catch(function () {
-            //Display alert for user 
-            AlertService.alerts.add('danger', 'Something went wrong', 20000);
-        }).finally(function () {
-            //Hide loader
-            $scope.isLoading = false;
-        });
+        $scope.search = function (value) {
 
+            //Set value 
+            $scope.searchValue = value;
+            if ($scope.searchValue) {
+                $scope.searchResultsText = $scope.searchValue
+                $scope.searchMethod = 'search';
+            } else {
+                $scope.searchResultsText = 'Most recent Flickr photos'
+                $scope.searchMethod = 'getRecent';
+            }
+
+            //Loading gallery
+            $scope.isLoading = true;
+            GalleryService.getGalleryConfig($scope.searchMethod, $scope.searchValue).then(function (response) {
+                //gallery infomation
+                $scope.gallery = response.photos;
+
+                if ($scope.gallery.total === '0') {
+                    AlertService.alerts.add('info', 'Try more general keywords', 2000);
+                }
+
+            }).catch(function () {
+                //Display alert for user 
+                AlertService.alerts.add('danger', 'Something went wrong', 5000);
+
+            }).finally(function () {
+                //Hide loader
+                $scope.isLoading = false;
+            });
+        };
     }
 ]);
--- app/scripts/components/gallery/gallery-service.js
+++ app/scripts/components/gallery/gallery-service.js
@@ -2,8 +2,8 @@
 galleryModule.service('GalleryService', ['config', '$http',
     function (config, $http) {
         return {
-            getGalleryConfig: function () {
-                return $http.get(config.apiUrl + '?method=flickr.photos.search&api_key=af8466c0c25daee7b10fa2cf58e40099&user_id=skyavisdesigns&format=json&nojsoncallback=1').then(function (response) {
+            getGalleryConfig: function (method, tags) {
+                return $http.get(config.apiUrl + '?method=flickr.photos.' + method + '&api_key=af8466c0c25daee7b10fa2cf58e40099&tags=' + tags + '&format=json&nojsoncallback=1').then(function (response) {
                     return response.data;
                 });
             }
