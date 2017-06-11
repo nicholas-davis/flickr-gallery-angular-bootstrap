e61153313b009e5d263063faf1b5130eaf54c2f2
Nick Davis <hire@nicholasdavis.co>
2017-06-11 02:39:56 -0400

    added intro cat photo

--- app/scripts/components/gallery/gallery-controller.js
+++ app/scripts/components/gallery/gallery-controller.js
@@ -2,22 +2,35 @@
 galleryModule.controller('GalleryController', ['$scope', '$state', '$stateParams', 'GalleryService', 'AlertService',
     function ($scope, $state, $stateParams, GalleryService, AlertService) {
 
+        //User search for photos
         $scope.search = function (value) {
 
-            //Set value 
-            $scope.searchValue = value;
-            if ($scope.searchValue) {
-                $scope.searchResultsText = $scope.searchValue
-                $scope.searchMethod = 'search';
+            //searchConfig 
+            $scope.searchConfig = {
+                method: null,
+                value: null,
+                quantity: 75,
+                text: null
+            };
+
+            //Set search value
+            $scope.searchConfig.value = value;
+            $scope.searchResultsText = null;
+            $scope.mostRecentText = null;
+
+            //If
+            if ($scope.searchConfig.value) {
+                $scope.searchResultsText = $scope.searchConfig.value
+                $scope.searchConfig.method = 'search';
             } else {
-                $scope.searchResultsText = 'Most recent Flickr photos'
-                $scope.searchMethod = 'getRecent';
+                $scope.mostRecentText = 'Most recent Flickr photos'
+                $scope.searchConfig.method = 'getRecent';
             }
 
             //Loading gallery
             $scope.isLoading = true;
-            GalleryService.getGalleryConfig($scope.searchMethod, $scope.searchValue).then(function (response) {
-                //gallery infomation
+            GalleryService.getGalleryConfig($scope.searchConfig.method, $scope.searchConfig.value, $scope.searchConfig.quantity).then(function (response) {
+                //gallery data
                 $scope.gallery = response.photos;
 
                 if ($scope.gallery.total === '0') {
@@ -33,5 +46,26 @@ galleryModule.controller('GalleryController', ['$scope', '$state', '$stateParams
                 $scope.isLoading = false;
             });
         };
+
+        //mostPopularConfig 
+        $scope.mostPopularConfig = {
+            method: 'search',
+            value: 'cat',
+            quantity: 1,
+        };
+
+        $scope.isLoading = true;
+        GalleryService.getGalleryConfig($scope.mostPopularConfig.method, $scope.mostPopularConfig.value, $scope.mostPopularConfig.quantity).then(function (response) {
+            //recent data
+            $scope.recent = response.photos;
+
+        }).catch(function () {
+            //Display alert for user 
+            AlertService.alerts.add('danger', 'Something went wrong', 5000);
+
+        }).finally(function () {
+            //Hide loader
+            $scope.isLoading = false;
+        });
     }
 ]);
